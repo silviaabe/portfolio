232d388c690d46e13debfdf6f396eb2699419097
Silvia Abe <silviaabebuzatto@gmail.com>
2024-06-25 15:51:59 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/silviaabe.png" alt="" />
-    <Titulo fontSize={20}>Silvia Abe</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Silvia Abe</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        silviaabe
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheira Front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
